ded10bde07bb77bf9d3640a637ee5ebb772822ae
Shiva Somapur <ssomapur@vmware.com>
2021-01-03 21:38:35 +0530

    Upload File script added

---
+++ upload-file.js
@@ -0,0 +1,18 @@
+require("chromedriver");
+const { Builder, By } = require("selenium-webdriver");
+const SeleniumUtils = require('./util');
+
+(async function example() {
+    let $browser = await new Builder().forBrowser("chrome").build();
+    try {
+        await $browser.get('http://softwaretestingplace.blogspot.com/2015/10/sample-web-page-to-test.html');
+
+        $browser.manage().window().maximize();
+
+        let browseEle = await $browser.findElement(By.id('uploadfile'));
+
+        browseEle.sendKeys('/Users/ssomapur/Desktop/Holiday\ Calendar.png');
+    } finally {
+        // $browser.quit();
+    }
+})();
